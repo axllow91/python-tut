00bb4664d6e8bac86f3df0c6036be17897c47a8d
axllow91 <dobremi91@gmail.com>
2019-02-07 21:45:06 +0200

    added delete functionality

--- files.py
+++ files.py
@@ -18,16 +18,25 @@ myFile.write('\nAnd I love C++')
 # Close file after we finished writting on it
 myFile.close()
 
+# To delete a file we need to import OS module
+# and then we can remove the file
+# import os
+# os.remove('myfile.txt')
+
 # Append to file
 myFile = open('myfile.txt', 'a') # a is for append something into file
 myFile.write(' I also like Typescript')
 myFile.close()
+
+
 
 # Read from file
 myFile = open('myfile.txt', 'r+') # r+ for reding from the file
 n_chars = 100 # max chars to read from the file
 text = myFile.read(n_chars) # reading the first 10 char
 print(text)
+
+
 
 
 
